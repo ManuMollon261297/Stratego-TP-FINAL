fa4e94d9ebcce24a417a1323ccd236df398ccc58
Ezequiel Vijande <evijande@itba.edu.ar>
2018-02-10 21:38:39 -0300

    Creo qe essta

--- Stratego/Stratego/Connecting.cpp
+++ Stratego/Stratego/Connecting.cpp
@@ -43,3 +43,36 @@ NetworkingState* Connecting::OnTimer(NetworkingModel* p_nwm, GameModel * Gm, Men
 	}
 	return p_state;
 }
+
+NetworkingState* Connecting::Name(NetWorkingEvent& ev, NetworkingModel* p_nwm, GameModel * Gm)
+{
+	NetworkingState* p_state = nullptr;
+
+	if (p_nwm->getServer() == CLIENT)
+	{
+		NetworkingState* p_state;
+		p_nwm->ResetTimeout(); //reinicio el timeout de comunicacion.
+		p_state = new WaitingNameResponse; //espero respuesta de que llego el paquete.
+		bool sent = false;
+		std::string my_name = (p_nwm->getMe());
+		unsigned char count = my_name.size();
+		std::string pckg;
+		pckg.append(1, NAME_IS_HEADER);
+		pckg.append(1, count);
+		pckg += my_name;
+		sent = p_nwm->sendPackage((char*)pckg.c_str(), pckg.size()); //Mando paquete con mi nombre
+		if (!sent) //Error de comunicacion.
+		{
+			Gm->SetExit(true);
+			p_state = new Quiting;
+		}
+		return p_state;
+	}
+	else
+	{
+		ErrorRoutine(p_nwm, Gm);
+		p_state = new Quiting;
+	}
+	return p_state;
+
+}
--- Stratego/Stratego/Connecting.h
+++ Stratego/Stratego/Connecting.h
@@ -8,4 +8,5 @@ class Connecting : public NetworkingState
 public:
 	Connecting();
 	virtual NetworkingState* OnTimer(NetworkingModel* p_nwm, GameModel * Gm, MenuModel* MM);
+	virtual NetworkingState* Name(NetWorkingEvent& ev, NetworkingModel* p_nwm, GameModel * Gm);
 };
